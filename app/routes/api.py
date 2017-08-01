1a8f21ea0f7f0344e5b1f943dbd2e768074251e6
andy-shi88 <shi88.andy@gmail.com>
2017-08-01 23:06:01 +0700

    #21 add route for order

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,10 +1,16 @@
 '''
 put api route in here
 '''
-from flask import Blueprint
+from flask import Blueprint, request
 
 # import middlewares
 from app.middlewares.authentication import token_required
+
+# import models
+from app.models.user import User
+
+# import controllers
+from app.controllers.order_controller import OrderController
 
 api = Blueprint('api', __name__)
 
@@ -13,3 +19,10 @@ api = Blueprint('api', __name__)
 @token_required
 def index(*args, **kwargs):
 	return 'api index'
+
+@api.route('/order', methods=['POST'])
+@token_required
+def order(*args, **kwargs):
+	# check if user is valid
+	user = User.verify_auth_token(kwargs['user'])
+	return OrderController.order(request)
